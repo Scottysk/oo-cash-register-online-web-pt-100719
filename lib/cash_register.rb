82399f4841d0f6f6d83f10fa018e824d797cf6aa
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-16 21:33:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,8 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
-    while quantity 
+    while quantity > 0
+    @items >> @title
   end
   
   def apply_discount
