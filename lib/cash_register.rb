f027a180c6a7c76c4e5e5f2f54f97c0929527671
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-16 21:44:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    @total += (price * quantity)
+    self.total += (price * quantity)
     while quantity > 0
     @items << title
     quantity -= 1
