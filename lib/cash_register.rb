27a41d03b4e8047c421e5a7f363f1c62bcd6548c
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-16 21:30:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
-
+class CashRegister
+  
+  attr_accessor :total, :price, :last_transaction
+  attr_reader :discount
+  
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total += (price * quantity)
+  end
+  
+  def apply_discount
+    @total = @total - @discount * 10
+    if @discount > 0
+    "After the discount, the total comes to #{@total}."
+    else 
+      "There is no discount to apply."
+    end
+  end
+  
+  
+end
